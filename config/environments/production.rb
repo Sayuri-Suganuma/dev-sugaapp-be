571d36b48c29366f646206ad0d657522b43f8217
Sayuri-Suganuma <sayuri.suganuma@o-g.co.jp>
2024-05-01 15:08:47 +0900

    production.rbにconfig.hostsを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "dev-sugaapp-be.sakuramobile.jp"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
